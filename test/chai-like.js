14d85f07e4d815826d063a010f1abed3568c7b7d
Liu Yang <zation1@gmail.com>
2015-03-26 16:34:41 +0800

    add chai-like matcher

---
+++ test/chai-like.js
@@ -0,0 +1,35 @@
+var chai = require('chai');
+var like = require('../index');
+chai.should();
+chai.use(like);
+
+describe('chai-like', function() {
+  it('should compare two JSON and ignore some keys based on expectation', function() {
+    var object = {
+      id: 1,
+      name: 'test',
+      updatedAt: 'now'
+    };
+    object.should.like({
+      name: 'test'
+    });
+  });
+
+  it('should deeply compare two JSON', function() {
+    var object = {
+      id: 1,
+      name: 'test',
+      product: {
+        id: 1,
+        name: 'product'
+      },
+      updatedAt: 'now'
+    };
+    object.should.like({
+      name: 'test',
+      product: {
+        name: 'product'
+      }
+    });
+  });
+});
